d9d2477a49442a9c5266dd47f5b1ceaee54f32c5
tjwjdgus12 <tjwjdgus12@cau.ac.kr>
2022-05-02 16:57:53 +0900

    Add go routine

--- net-hw3/MultiClientTCPServer.go
+++ net-hw3/MultiClientTCPServer.go
@@ -1,5 +1,5 @@
 /**
-   TCPServer.go
+   MultiClientTCPServer.go
    by Jeong-Hyeon Seo (20172864)
 **/
 
@@ -53,48 +53,49 @@ func main() {
 		conn, _ := listener.Accept()
 		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
 
-	L1:
-		for {
-
-			// Wait for command input
-			count, _ := conn.Read(buffer)
-			if count == 0 {
-				continue
-			}
-			optionNum := string(buffer[:count])
-			fmt.Printf("Command %s\n", optionNum)
-
-			// Process request
-			var response string
-
-			switch optionNum {
-
-			case "1": // send text converted to UPPER-case
+		go func() {
+		L1:
+			for {
+				// Wait for command input
 				count, _ := conn.Read(buffer)
-				response = strings.ToUpper(string(buffer[:count]))
+				if count == 0 {
+					continue
+				}
+				optionNum := string(buffer[:count])
+				fmt.Printf("Command %s\n", optionNum)
 
-			case "2": // send client's IP address and port number
-				clientAddr := strings.Split(conn.RemoteAddr().String(), ":")
-				response = fmt.Sprintf("clinet IP = %s, port = %s\n", clientAddr[0], clientAddr[1])
+				// Process request
+				var response string
 
-			case "3": // send server request count
-				response = fmt.Sprintf("requests served = %d\n", req_cnt)
+				switch optionNum {
 
-			case "4": // send server running time
-				HHMMSS := duration2HHMMSS(time.Since(startTime))
-				response = fmt.Sprintf("run time = %s\n", HHMMSS)
+				case "1": // send text converted to UPPER-case
+					count, _ := conn.Read(buffer)
+					response = strings.ToUpper(string(buffer[:count]))
 
-			case "5": // close connection
-				conn.Close()
-				break L1
+				case "2": // send client's IP address and port number
+					clientAddr := strings.Split(conn.RemoteAddr().String(), ":")
+					response = fmt.Sprintf("clinet IP = %s, port = %s\n", clientAddr[0], clientAddr[1])
 
-			default: // exception
-				response = "Invalid Input!\n"
+				case "3": // send server request count
+					response = fmt.Sprintf("requests served = %d\n", req_cnt)
+
+				case "4": // send server running time
+					HHMMSS := duration2HHMMSS(time.Since(startTime))
+					response = fmt.Sprintf("run time = %s\n", HHMMSS)
+
+				case "5": // close connection
+					conn.Close()
+					break L1
+
+				default: // exception
+					response = "Invalid Input!\n"
+				}
+
+				conn.Write([]byte(response))
+				req_cnt++
 			}
-
-			conn.Write([]byte(response))
-			req_cnt++
-		}
-		fmt.Printf("\n")
+			fmt.Printf("\n")
+		}()
 	}
 }
